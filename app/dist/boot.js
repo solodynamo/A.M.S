256d7691f7f11348f097eba71b2cd411a5f4db95
solodynamo <bittuf3@gmail.com>
2016-10-04 23:18:38 +0530

    Live-server added and minor changes

--- app/dist/boot.js
+++ app/dist/boot.js
@@ -1,2 +1,4 @@
 /// <reference path="_all.ts" />
+var a = [3, 4, 5, 4];
+var b = a.concat([6, 7, 8]);
 //# sourceMappingURL=boot.js.map
--- app/src/boot.ts
+++ app/src/boot.ts
@@ -1,2 +1,6 @@
 /// <reference path="_all.ts" />
 
+let a = [3,4,5,4];
+let b = [...a,6,7,8];
+
+
