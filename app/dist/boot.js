d323162fd0a19afd5ac0ab20084fca8bf1c90e8a
solodynamo <bittuf3@gmail.com>
2016-10-10 23:28:48 +0530

    Project ported from TS and minor changes

--- app/dist/boot.js
+++
@@ -1,4 +0,0 @@
-/// <reference path="_all.ts" />
-var a = [3, 4, 5, 4];
-var b = a.concat([6, 7, 8]);
-//# sourceMappingURL=boot.js.map
--- app/src/boot.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="_all.ts" />
-
-let a = [3,4,5,4];
-let b = [...a,6,7,8];
-
-
